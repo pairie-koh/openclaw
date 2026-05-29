21b6e8fd94f59ec5c3c8a77672e9c500bf84d56b
Peter Steinberger <steipete@gmail.com>
2026-05-29 15:15:07 -0400

    chore: remove generated artifacts

--- packages/gateway-client/dist/readiness.mjs
+++
@@ -1,3 +0,0 @@
-import "./timeouts.mjs";
-import { t as startGatewayClientWhenEventLoopReady } from "./readiness-DIrwMTRN.mjs";
-export { startGatewayClientWhenEventLoopReady };
--- packages/gateway-protocol/dist/version.mjs
+++
@@ -1,6 +0,0 @@
-//#region packages/gateway-protocol/src/version.ts
-const PROTOCOL_VERSION = 4;
-const MIN_CLIENT_PROTOCOL_VERSION = 4;
-const MIN_PROBE_PROTOCOL_VERSION = 4;
-//#endregion
-export { MIN_CLIENT_PROTOCOL_VERSION, MIN_PROBE_PROTOCOL_VERSION, PROTOCOL_VERSION };
